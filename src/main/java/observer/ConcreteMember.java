b3c1a3a916051d1e4c08fbfd60d124c7b4af20bf
ravidkrisi <ravidkrisi22@gmail.com>
2022-12-26 16:54:03 +0200

    part 1 before Tests

--- src/main/java/observer/ConcreteMember.java
+++ src/main/java/observer/ConcreteMember.java
@@ -2,10 +2,10 @@ package observer;
 
 public class ConcreteMember implements Member
 {
-
+    public UndoableStringBuilder client;
     @Override
     public void update(UndoableStringBuilder usb)
     {
-
+    this.client=usb;
     }
 }
--- src/main/java/observer/GroupAdmin.java
+++ src/main/java/observer/GroupAdmin.java
@@ -4,40 +4,54 @@ import java.util.ArrayList;
 
 public class GroupAdmin implements Sender
 {
-    private ArrayList <ConcreteMember>
+    private ArrayList <Member> clients = new ArrayList<Member>();
+    private UndoableStringBuilder mode = new UndoableStringBuilder();
+
     @Override
+    // add a new client to the clients list
     public void register(Member obj)
     {
-
+    this.clients.add(obj);
     }
 
     @Override
+    // gets a client and removes it from the clients list
     public void unregister(Member obj)
     {
-
+    this.clients.remove(obj);
     }
 
     @Override
+    //Inserts the string into this character sequence.
     public void insert(int offset, String obj)
     {
+    this.mode.insert(offset, obj);
 
     }
 
     @Override
     public void append(String obj)
     {
-
+    this.mode.append(obj);
     }
 
     @Override
     public void delete(int start, int end)
     {
-
+    this.mode.delete(start, end);
     }
 
     @Override
     public void undo()
     {
+    this.mode.undo();
+    }
 
+    public void notifyClients()
+    {
+        for(Member client : this.clients)
+        {
+            client.update(this.mode);
+        }
     }
 }
